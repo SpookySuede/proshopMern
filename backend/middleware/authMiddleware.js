726297b2eafeef423a29d347709387cc077950e8
SpookySuede <spookysuede@gmail.com>
2021-02-17 08:43:38 -0500

    Removed redundant conditional if
    Was causing a 401 issue, changed to else statement

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,20 +4,24 @@ import User from '../models/userModel.js';
 
 const protect = asyncHandler(async (req, res, next) => {
     let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')
+    if(req.headers.authorization &&
+       req.headers.authorization.startsWith('Bearer')
     ) {
         try {
             const token = req.headers.authorization.split(' ')[1];
+
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
             req.user = await User.findById(decoded.id).select('-password');
             next();
+            
         } catch (error) {
             console.error(error)
             res.status(401);
             throw new Error('Not authorized, token failed')
         }
     } 
-    if(!token) {
+    else {
         res.status(401);
         throw new Error('Not authorized.')
     }
