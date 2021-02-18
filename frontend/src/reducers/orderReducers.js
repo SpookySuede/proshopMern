aa15ffc76ce0ef17c126c56464c9f7fcbc2ee513
SpookySuede <spookysuede@gmail.com>
2021-02-18 17:25:06 -0500

    Update orderReducers.js
    added orderDetailsReducer and orderPayReducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,7 +2,14 @@ import {
  CREATE_ORDER_FAILED,
  CREATE_ORDER_REQUEST,
  CREATE_ORDER_SUCCESS,
-} from '../constants/orderconstants';
+ DETAILS_ORDER_FAILED,
+ DETAILS_ORDER_REQUEST,
+ DETAILS_ORDER_SUCCESS,
+ PAY_ORDER_FAILED,
+ PAY_ORDER_REQUEST,
+ PAY_ORDER_RESET,
+ PAY_ORDER_SUCCESS,
+} from "../constants/orderconstants";
 
 export const orderCreateReducer = (state = {}, action) => {
  switch (action.type) {
@@ -21,6 +28,57 @@ export const orderCreateReducer = (state = {}, action) => {
     loading: false,
     error: action.payload,
    };
+  default:
+   return state;
+ }
+};
+
+export const orderDetailsReducer = (
+ state = { loading: true, orderItems: [], shippingAddress: {} },
+ action
+) => {
+ switch (action.type) {
+  case DETAILS_ORDER_REQUEST:
+   return {
+    ...state,
+    loading: true,
+   };
+  case DETAILS_ORDER_SUCCESS:
+   return {
+    loading: false,
+    order: action.payload,
+   };
+  case DETAILS_ORDER_FAILED:
+   return {
+    loading: false,
+    error: action.payload,
+   };
+  default:
+   return state;
+ }
+};
+
+export const orderPayReducer = (
+ state = { },
+ action
+) => {
+ switch (action.type) {
+  case PAY_ORDER_REQUEST:
+   return {
+    loading: true,
+   };
+  case PAY_ORDER_SUCCESS:
+   return {
+    loading: false,
+    success: true,
+   };
+  case PAY_ORDER_FAILED:
+   return {
+    loading: false,
+    error: action.payload,
+   };
+   case PAY_ORDER_RESET:
+     return {}
   default:
    return state;
  }
