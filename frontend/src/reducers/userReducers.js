43641e6f1917361c8578328242d26d5ccc9b5542
SpookySuede <spookysuede@gmail.com>
2021-02-28 09:08:56 -0500

    Create User List reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -16,7 +16,11 @@ import {
  USER_UPDATE_REQUEST,
  USER_UPDATE_SUCCESS,
  USER_UPDATE_FAILED,
- USER_DETAILS_RESET
+ USER_DETAILS_RESET,
+ USER_LIST_REQUEST,
+ USER_LIST_SUCCESS,
+ USER_LIST_FAILED,
+ USER_LIST_RESET
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -85,6 +89,21 @@ export const userUpdateReducer = (state = { user: {} }, action) => {
    return { loading: false, success: true };
   case USER_UPDATE_FAILED:
    return { loading: false, error: action.payload };
+  default:
+   return state;
+ }
+};
+
+export const userListReducer = (state = { users: [] }, action) => {
+ switch (action.type) {
+  case USER_LIST_REQUEST:
+   return { loading: true };
+  case USER_LIST_SUCCESS:
+   return { loading: false, users: action.payload };
+  case USER_LIST_FAILED:
+   return { loading: false, error: action.payload };
+  case USER_LIST_RESET:
+    return { users: [] }
   default:
    return state;
  }
